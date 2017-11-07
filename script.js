2dc4e8af5711c50b95781d216a07234b3d3acec3
Kieran Kuhn <kuhn.kieran@gmail.com>
2017-11-06 20:48:29 -0700

    Added input validation

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function addButtons(name){
 	var newButton = $("<button>");
 	newButton.addClass("btn btn-primary");
 	newButton.attr("name", name);
-	newButton.text(name);
+	newButton.text(name.toUpperCase());
 	//Display the button
 	$("#button-area").append(newButton);
 }
@@ -16,11 +16,39 @@ function addNewButton(name){
 	var newButton = $("<button>");
 	newButton.addClass("btn btn-primary");
 	newButton.attr("name", name);
-	newButton.text(name);
+	newButton.text(name.toUpperCase());
 	//Clear input area
 	$("#gif-name").val("");
 	//Display the button
 	$("#button-area").append(newButton);
+}
+//Validate input
+function validateInput(input){
+	//If search area is empty
+	if(input === ""){
+		errorMessage();
+		return false;
+	}
+	//Array with valid input characters
+	var validInputs = "abcdefghijklmnopqrstuvwxyz1234567890".split("");
+	//Check each character of input
+	for(var i=0;i<input.length;i++){
+		//If a character in input does not exist in valid inputs
+		if(validInputs.indexOf(input.charAt(i)) === -1){
+			errorMessage();
+			return false;
+		}
+	}
+	//If all is well, return true
+	return true;
+}
+//Error message for invalid input
+function errorMessage(){
+	//Clear input area
+	$("#gif-name").val("");
+	//Display the error message
+	$("#error-message").html("<p class='alert alert-danger'>Input can only contain letters A-Z and numbers 1-9");
+	event.preventDefault();
 }
 
 $(document).ready(function(){
@@ -31,8 +59,11 @@ $(document).ready(function(){
 	}
 	//When form-1 is submitted 
 	$("#form-1").submit(function(){
-		//Add a new button
-		addNewButton($("#gif-name").val());
-		event.preventDefault();
+		//If input is valid
+		if(validateInput($("#gif-name").val())){
+			//Add a new button
+			addNewButton($("#gif-name").val());
+			event.preventDefault();
+		}
 	});
 });
